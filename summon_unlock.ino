b943531ebc764557c6e62ff1582c93ac7462c060
XBmod <xmicky@hotmail.fr>
2026-08-28 19:32:40 +0200

    Fix TLSSC injection gate

--- summon_unlock.ino
+++ summon_unlock.ino
@@ -238,7 +238,7 @@ static void injectTLSSC(const twai_message_t &src) {
     fmode = forceMode;
     portEXIT_CRITICAL(&stateMux);
 
-    if ((!en || !gate) && !fmode)
+    if ((!en || !gate))
         return;
 
     doInjectTLSSC(src.data, src.data_length_code);
